41bfb4860c4416739a202eff70806ee9964a3308
Alexandr Novikov <shura_novikov@mail.ru>
2023-11-20 16:14:12 +0300

    less4-4

--- lesson-02/04.js
+++ lesson-02/04.js
@@ -2,7 +2,7 @@ const giveMeRightCondition = (value1, value2, maximum) => {
     if ( value1 === value2 && value1 <= maximum) {
         console.log('Success!');
     } else {
-        console.log('Something wrong!');
+        console.log("'Something wrong!'");
     }
 }
 giveMeRightCondition(300, 100, 200)
