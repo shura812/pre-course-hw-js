6e2ecdcbb38ddfd189c079ebea4020075eb8ec15
Alexandr Novikov <shura_novikov@mail.ru>
2023-11-20 16:03:31 +0300

    less33-3

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -2,7 +2,7 @@ const giveMeRightNumders = (value1, value2, maximum) => {
     if(value1 === value2 || value1 <= maximum) {
         console.log('Success!');
     } else {
-        console.log('Something wrong!');
+        console.log("'Something wrong!'");
     }
 }
-giveMeRightNumders(300, 100, 200);
+giveMeRightNumders(350, 100, 200);
