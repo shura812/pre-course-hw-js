b510aa99cfb907aabb18aed9bbb4e65c43f914bd
Alexandr Novikov <shura_novikov@mail.ru>
2023-11-20 17:18:20 +0300

    less7-7

---
+++ lesson-02/07.js
@@ -0,0 +1,11 @@
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+passportMarried.married = true;
+console.log(passportMarried);
