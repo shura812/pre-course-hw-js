04bfebcda54ff00623aadf194941bfa4303d3dd3
Alexandr Novikov <shura_novikov@mail.ru>
2023-11-20 16:48:34 +0300

    less6-6

---
+++ lesson-02/06.js
@@ -0,0 +1,11 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+console.log(passportWithAddress.address.city);
+passportWithAddress.address.city = "Bobryisk";
+console.log(passportWithAddress.address.city);
