7ab69720c58eeee48ac7a51990141f42a47b5e1e
Alexandr Novikov <shura_novikov@mail.ru>
2023-11-28 19:41:19 +0300

    less3-1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,7 @@
+let colors = ['синий','черный','зеленый','красный','желтый','зеленый'];
 
+function createColorString(){
+return colors.filter(colors => colors === 'черный' || colors === 'красный' || colors ==='желтый').join('-');
+}
+
+console.log(createColorString());
