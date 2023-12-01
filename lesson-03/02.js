ac3400444235473a2f30123c1124167c56da5147
Alexandr Novikov <shura_novikov@mail.ru>
2023-12-01 22:33:11 +0300

    less3-2

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,19 +1,6 @@
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
-
-if (words.indexOf("апельсин") !== -1) {
-    console.log('Ура! нашел');
-} else {
-    console.log('Придется поискать в другом магазине. . .');
+if(words.indexOf("апельсин") !==-1) {
+ console.log('Ура! нашел');
+}  else {
+    console.log('Придется поискать в другом магазине…');
 }
-
-/*const words = ["яблоки" , "банан", "апельсин" , "манго" , "киви"];
-
-const findWord = (word) => {
-    if (words.indexOf(word) > -1) {
-        console.log('Ура! нашел');
-    } else {
-        console.log('Придется поискать в другом магазине...')
-    }
-};
-findWord('апельсин');
-findWord('картошка');*/
