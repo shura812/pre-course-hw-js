ed89f5c2552b6202066a2419becad6531281a8ac
Alexandr Novikov <shura_novikov@mail.ru>
2023-12-01 22:01:56 +0300

    lesi3-2

--- src/components/filter-panel/search-button.js
+++
@@ -1,8 +0,0 @@
-export function renderSearchButton() {
-    const searchButton = document.createElement('button');
-    searchButton.innerText = 'Search';
-    searchButton.addEventListener('click', () => {
-        setSearchTerm(searchInput.value);
-    });
-    return searchButton;
-}
