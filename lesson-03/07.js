d61cc992dbe4d3889d3708f8823fb69a489fc096
Alexandr Novikov <shura_novikov@mail.ru>
2023-12-05 15:19:38 +0300

    less3-7

---
+++ lesson-03/07.js
@@ -0,0 +1,11 @@
+const numbers = [
+    [1, 2, 3, 4, 5],
+    [11, 20, 33, 40, 55],
+    [111, 200, 333, 400, 555],
+];
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i] = numbers[i].filter(function (n) {
+        return n % 2 === 0
+    });
+}
+console.log(numbers);
